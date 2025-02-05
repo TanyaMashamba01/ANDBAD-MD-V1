56dbd369d9f3519fbce5830dd7e58de2e1e64668
TanyaMashamba01 <mildredtanyamashamba01@gmail.com>
2025-02-06 01:41:10 +0200

    Update ping.js

--- Andbad_cmds/ping.js
+++ Andbad_cmds/ping.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "ping", reaction: "🫀", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "ping", reaction: "🇿🇼❤️‍🔥", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'AM ALIVE STUPID.... \n\n\nGO GITHUB AND SEARCH 𝚫𝚴𝐃𝚩𝚫𝐃 MD V1 FORK THE REPO DEPLOY AND DM FOR YOURE 15$\n\n\n';
+    let z = 'AM ALIVE STUPID.... \n\n\nGO GITHUB AND SEARCH 𝚫𝚴𝐃𝚩𝚫𝐃 MD FORK THE REPO DEPLOY AND DM FOR YOURE 15$\n\n\n';
     let d = '                                                                           I LOVE MY OWNER 𝚳𝚪 𝚫𝚴𝐃𝚩𝚫𝐃';
     let varmess = z + d;
-    var video = 'https://i.imgur.com/UfpoNZT.mp4';
+    var video = 'https://imgur.com/a/8Blk9l0.mp4';
     await zk.sendMessage(dest, { video: { url: img }, caption: varmess });
     //console.log("montest")
 });
